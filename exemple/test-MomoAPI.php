05ed0193e8de66d959cb5e25583c54b405fce4b8
Wanzoou <wanzoou@gmail.com>
2024-06-11 11:25:35 +0100

    Create test exemple

--- exemple/test-MomoAPI.php
+++ exemple/test-MomoAPI.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once 'MomoAPI.php';
+require_once '../MomoAPI.php';
 
 $API = new MomoAPI();
 
 $API->setMomoPayHost('sandbox.momodeveloper.mtn.com');
 $API->setTargetEnvironment('sandbox');
-$API->setPrimaryKey('6e55179af70c4e06b152338de11c6cd3');
+$API->setPrimaryKey('6e5510c4e06b152338fpl24de11c6cd3');
 $API->setProviderCallbackHost('');
 
 $API->requestToPay('237679465319', 1000, 'EUR');
